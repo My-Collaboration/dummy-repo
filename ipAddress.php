36985059ad5de31fe960ff89eb1112b6af33c9a2
Himanshu Yadav <ydv1608@gmail.com>
2020-10-12 02:26:50 +0530

    send through real -u

--- ipAddress.php
+++ ipAddress.php
@@ -4,7 +4,7 @@ include 'class.IPInfoDB.php';
 
 // Load the class
 $ApiKey = 'Another API';
-//call to fn
+//call to fn sending to -u
 $ipinfodb = new IPInfoDB(ApiKey);
 $results = $ipinfodb->getCity('Random IP');
 
